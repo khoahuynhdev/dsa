921b62cc45bfd8051e66f6233c54d1994c946da7
khoahuynhdev <khoa0319@gmail.com>
2021-01-06 20:34:22 +0700

    add bfs

--- bst.ts
+++ bst.ts
@@ -19,16 +19,44 @@ class BSTTree {
   private addToTree(node: BSTNode | null, value: number): BSTNode {
     if (!node) return new BSTNode(value);
 
-    if (value > node.value)
-      return node.right = this.addToTree(node.right, value);
+    if (value > node.value) {
+      node.right = this.addToTree(node.right, value);
+    }
 
-    if (value < node.value)
-      return node.left = this.addToTree(node.left, value);
+    if (value < node.value) {
+      node.left = this.addToTree(node.left, value);
+    }
 
     return node;
   }
 
-  add(value: number): BSTNode {
-    return this.addToTree(this.root, value);
+  add(value: number): void {
+    this.addToTree(this.root, value);
   }
+
+  bfs(action: Function): void {
+    if (!this.root) return;
+    const queue: BSTNode[] = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      action(node?.value);
+      if (node?.left) queue.push(node.left);
+      if (node?.right) queue.push(node.right);
+    }
+  }
+
+
 }
+
+const tree = new BSTTree(new BSTNode(7));
+tree.add(1);
+tree.add(11);
+tree.add(3);
+tree.add(2);
+tree.add(5);
+tree.add(8);
+tree.add(0);
+tree.add(10);
+tree.add(4);
+
+tree.bfs(console.log);
